f5433cbcd8ec32c500561ff8e64f3114c9fa75a9
Lihan <b0926920221@gmail.com>
2025-11-25 20:18:37 +0800

    前端驗證roleId、渲染基本fullalendar到employee.cshtml

--- Scheduling/Controllers/DashboardController.cs
+++ Scheduling/Controllers/DashboardController.cs
@@ -4,13 +4,30 @@ namespace Scheduling.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Boss()
         {
+            if (HttpContext.Session.GetInt32("RoleId") != 1)
+            {
+                return RedirectToAction("Index", "Login");
+            }
             return View();
         }
-        public IActionResult Boss() => View();
-        public IActionResult Manager() => View();
-        public IActionResult Employee() => View();
 
+        public IActionResult Manager()
+        {
+            if (HttpContext.Session.GetInt32("RoleId") != 2)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+        public IActionResult Employee()
+        {
+            if (HttpContext.Session.GetInt32("RoleId") != 3)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
     }
 }
---
+++ Scheduling/Controllers/ScheduleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scheduling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        //[HttpGet]
+        //public IActionResult GetAll()
+        //{
+
+        //}
+    }
+}
