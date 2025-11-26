d06fdaebd1f8cd2c87a30b32e07a9d1badbc3d8c
Lihan <b0926920221@gmail.com>
2025-11-26 18:48:26 +0800

    新增共用的日期方法類別和控制器、設計日曆完成

---
+++ Scheduling/Helpers/loadHolidays.cs
@@ -0,0 +1,15 @@
+﻿namespace Scheduling.Helpers
+{
+    public static class HolidayHelper
+    {
+        public static async Task<string> LoadHolidaysAsync(int year,IWebHostEnvironment env) {
+            string fileName = $"holidays{year}.json";
+            string path = Path.Combine(env.WebRootPath,"js",fileName);
+            if (!File.Exists(path)) return "[]";
+            using var fileStream = new FileStream(path,FileMode.Open,FileAccess.Read);
+            using var reader = new StreamReader(fileStream);
+            string json =  await reader.ReadToEndAsync();
+            return json;
+        }
+    }
+}
