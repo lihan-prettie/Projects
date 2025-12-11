8a7af0f69d6662a7f887bf72139eef3d40ba59e8
Lihan <b0926920221@gmail.com>
2025-12-11 14:55:38 +0800

    反向工程、基本登入頁面實做

---
+++ Shopping/Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sigup()
+        {
+            return View();
+        }
+    }
+}
